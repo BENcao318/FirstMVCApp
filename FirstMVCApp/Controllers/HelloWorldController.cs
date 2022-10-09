6117327323d63f6242c417bc0a3b9fdd2f865921
BENcao318 <bc6016@mun.ca>
2022-10-09 16:06:06 -0400

    Add controllers and views

---
+++ FirstMVCApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstMVCApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Hello()
+        {
+            return View();
+        }
+    }
+}
